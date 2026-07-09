d7beb5d9fee538b10a2839d999a8e97104059ed7
seaikh91-Asif <seaikh91@gmail.com>
2026-07-09 23:37:10 +0530

    initializing props concept

--- Third_TelwindProps_ReactProject/src/App.jsx
+++ Third_TelwindProps_ReactProject/src/App.jsx
@@ -12,6 +12,23 @@ function App() {
      <h2 className='bg-green-200'>
         hello world
      </h2>
+
+     
+
+<div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="https://dyl347hiwv3ct.cloudfront.net/app/uploads/2025/11/logan-weaver-lgnwvr-GXQP77GIsIU-unsplash-small.webp" />
+  </div>
+  <div class="flex items-center md:items-start">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
     </>
   )
 }
